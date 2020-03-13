a76e602aebaa82fbd879b039a25be0bf35c6e519
Developer <juan.bautistarey@nube.unadmexico.mx>
2020-03-13 17:58:24 -0600

    agregando dependencias

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, name: 'course 1'},
+    {id: 2, name: 'course 2'},
+    {id: 3, name: 'course 3'}
+];
+
 
 app.get('/', (req, res)=>{
     res.send('Hola que hace!!');
 });
-
 app.get('/api/courses', (req, res)=>{
-    res.send([1,2,3,4]);
+    res.send(courses);
+});
+app.get('/api/courses', (req, res)=>{
+    
+    const {error}= validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
-app.get('/api/posts/:year/:mont', (req,res)=>{
-    res.send(req.params);
+app.put('/api/courses/:id',(req,res)=>{
+    //Look up the course
+    //If not existing, return 404
+    const course = courses.find(c=> c.id===parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    //Validate 
+    //If invalid, return 400 - Bad request
+    const {error}= validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    //Update course
+    //Return the update course    
+    course.name = req.body.name;
+    res.send(course);
 });
+
+app.get('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c=> c.id===parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+});
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 4000;
 
